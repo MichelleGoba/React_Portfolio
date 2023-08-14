b7fc90cbc3d699eda90bcfa3465bd031d30de577
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-14 13:58:00 +0200

    home conten and image

--- Portfolio/src/components/home/Home.jsx
+++ Portfolio/src/components/home/Home.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import "./home.css";
-// import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import myLogo from '../../images/logo.png';
+import Row from 'react-bootstrap/Row';
 
 
 
 const Home = () => {
   return (
-    <Container>
-      <div className='content'>
-        <h1>Hi! im <span>M</span>ichelle</h1>
-        <h3 className='my-profession'>Im a <span className='typing'>Junior Software Developer</span></h3>
+    <Container className='home-section'>
+      <Row>
+        <Col lg={6} md={4} className='content'>
+          <h1>Hi! Im <span>M</span>ichelle</h1>
+          <h3 className='my-profession'>Im a <span className='typing'>Junior Software Developer</span></h3>
+          <p>I am a web developer from Cape Town who is learning how to navigate the development processes. My area of competence includes designing websites, applications, and many more...</p>
+          <a href='#about'className='btn'>More about Me</a>
+        </Col>
 
-      </div>
-      <div className='image'>
-        <img src= {myLogo} className='home-image'/>
-      </div>
-    </Container>
+        <Col xs={6} md={6} className='image'>
+          <img src= {myLogo} className='home-image' />
+        </Col>
+      </Row>
+    </Container> 
 
   )
 }
