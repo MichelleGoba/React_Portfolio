b92f3d969f832a58737aa55270095944f8c94832
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-14 14:25:20 +0200

    home image border alignment

--- Portfolio/src/components/home/Home.jsx
+++ Portfolio/src/components/home/Home.jsx
@@ -12,14 +12,14 @@ const Home = () => {
     <Container className='home-section'>
       <Row>
         <Col lg={6} md={4} className='content'>
-          <h1>Hi! Im <span>M</span>ichelle</h1>
+          <h1 className='greeting'>Hi! Im <span>M</span>ichelle</h1>
           <h3 className='my-profession'>Im a <span className='typing'>Junior Software Developer</span></h3>
-          <p>I am a web developer from Cape Town who is learning how to navigate the development processes. My area of competence includes designing websites, applications, and many more...</p>
+          <p>I am a Junior Software Developer from Cape Town who is learning how to navigate the development processes. My area of competence includes designing websites, applications, and many more...</p>
           <a href='#about'className='btn'>More about Me</a>
         </Col>
 
-        <Col xs={6} md={6} className='image'>
-          <img src= {myLogo} className='home-image' />
+        <Col xs={6} md={6} className='home-image'>
+          <img src= {myLogo} className='image' />
         </Col>
       </Row>
     </Container> 
