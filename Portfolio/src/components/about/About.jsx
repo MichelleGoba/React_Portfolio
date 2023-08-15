b2393efb64dec0a35356e7dad655c0bc16c73693
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-15 15:55:45 +0200

    about section start

--- Portfolio/src/components/about/About.jsx
+++ Portfolio/src/components/about/About.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import aboutImage from '../../images/abt-img.png';
 import "./about.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 const About = () => {
   return (
-    <div>About</div>
+    <Container className='about-section'>
+      <h2 className='about-title'>About Me</h2>
+      <Row>
+        {/*about image start*/}
+        <Col xs={6} md={6} className='about-image'>
+          <img src= {aboutImage} className='image' />
+        </Col>
+        {/* about image end */}
+
+        <Col lg={6} md={4} className='content'>
+          
+        </Col>
+
+        
+      </Row>
+    </Container> 
+
   )
 }
 
