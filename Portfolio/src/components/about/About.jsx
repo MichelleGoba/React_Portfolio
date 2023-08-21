374a82236355de73354ad9f4d30a4b4b210e4f0e
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-21 15:50:30 +0200

    accomplishments

--- Portfolio/src/components/about/About.jsx
+++ Portfolio/src/components/about/About.jsx
@@ -48,19 +48,19 @@ const About = () => {
         </Col>
       </Row> 
 
-      <Container className='container-fluid skills' id='skills-section'>
+      <Container className='container-fluid skills' id='skills'>
         <h3 className='skills-title'>Skills</h3>
-        <Row>
-        <Col xs={6} md={6} w-100 className='skills-content'>
+        <Row className='skills-row'>
+        <Col xs={6} md={6} className='skills-content '>
           <p>My skills include designing and developing features and functionalities of websites and applications using Balsamiq, Figma, HTML, CSS, JavaScript, Node JS, React JS, and Python.</p>
-          <p>
+          {/* <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, perferendis at. Harum assumenda sint commodi, sit fugiat amet odio, officiis facilis voluptas quam pariatur eius quasi blanditiis voluptatem quod at.
           </p>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio minus et quod, rerum veritatis at, aliquid excepturi asperiores harum quisquam sed animi beatae deleniti exercitationem doloremque eum. Quaerat, veritatis corrupti?
-          </p>
+          </p> */}
         </Col> 
-        <Col xs={6} md={6} w-100 className='skills-images'>
+        <Col lg={6} md={4} w-100 className='skills-images'>
           <div className='div-img1'>
             <div className='firstImg'>
               <img src= {image4} className='image4'/>
@@ -83,6 +83,109 @@ const About = () => {
         </Col> 
 
         </Row>
+
+        <Container className='container-fluid skills' id='education'>
+          <div className='accomplishments'>
+            <div className="education-section">
+                    <h3 className="title">Education</h3>
+                    <div className="row">
+                      <div className="timeline-box padd-15">
+                        <div className="timeline shadow-dark">
+                          {/* <!-- ===== timeline item ===== --> */}
+                          <div className="timeline-item">
+                            <div className="circle-dot"></div>
+                            <h3 className="timeline-date">
+                              <i className="fa fa-calendar"></i> 2023 - Current
+                            </h3>
+                            <h4 className="timeline-title">Udemy</h4>
+                            <h5>The Complete 2023 Web Development Bootcamp</h5>
+                            <p className="timeline-text">In this course I am learning to code and become a full-stack web developer.</p>
+                          </div>
+                          {/* <!-- ===== timeline item ===== --> */}
+                          <div className="timeline-item">
+                            <div className="circle-dot"></div>
+                            <h3 className="timeline-date">
+                              <i className="fa fa-calendar"></i> 2019 - 2020
+                            </h3>
+                            <h4 className="timeline-title">UCT</h4>
+                            <h5>BSc Honors in GIS</h5>
+                            <p className="timeline-text">The degree provided me with the practical skills and academic understanding required to integrate GIS concepts into my daily job routine. Natural, geographical, and computer sciences were the center of the field.</p>
+                          </div>
+                              {/* <!-- ===== timeline item ===== --> */}
+                              <div className="timeline-item">
+                                <div className="circle-dot"></div>
+                                <h3 className="timeline-date">
+                                  <i className="fa fa-calendar"></i> 2015 - 2016
+                                </h3>
+                                <h4 className="timeline-title">CPUT</h4>
+                                <h5>BTech in Environmental Management</h5>
+                                <p className="timeline-text">This degree prepared me for responsible positions which require knowledge of environmental impact assessments and environmental management systems.</p>
+                              </div>
+                                  {/* <!-- ===== timeline item ===== --> */}
+                          <div className="timeline-item">
+                            <div className="circle-dot"></div>
+                            <h3 className="timeline-date">
+                              <i className="fa fa-calendar"></i> 2012 - 2015
+                            </h3>
+                            <h4 className="timeline-title">CPUT</h4>
+                            <h5>ND Environmental Management</h5>
+                            <p className="timeline-text">This degree prepared me for responsible positions which require knowledge of environmental impact assessments and environmental management systems.</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='experience-section'>
+                    <h3 className="title">Experience</h3>
+                    <div className="row">
+                      <div className="timeline-box padd-15">
+                        <div className="timeline shadow-dark">
+                          {/* <!-- ===== timeline item ===== --> */}
+                          <div className="timeline-item">
+                            <div className="circle-dot"></div>
+                            <h3 className="timeline-date">
+                              <i className="fa fa-calendar"></i> 2023 - Current
+                            </h3>
+                            <h4 className="timeline-title">UVU Africa</h4>
+                            <h5>Software Development Intern</h5>
+                            <p className="timeline-text">I'm responsible for using HTML, CSS, and JavaScript to construct, develop, and maintain websites. I put into practice the front-end logic that controls how web applications' visual
+                              components behave. I am also in charge of coding, designing, and deploying development
+                              initiatives on the server and client side. I work with developers to design algorithms and
+                              flowcharts.
+                              </p>
+                          </div>
+                              {/* <!-- ===== timeline item ===== --> */}
+                              <div className="timeline-item">
+                                <div className="circle-dot"></div>
+                                <h3 className="timeline-date">
+                                  <i className="fa fa-calendar"></i> 2021 - 2022
+                                </h3>
+                                <h4 className="timeline-title">Steam House Engineering</h4>
+                                <h5>Safety Officer</h5>
+                                <p className="timeline-text">In this position, was in charge of assuring the company's total compliance with safety, health, and environmental laws, regulations, policies, procedures, and legislation.</p>
+                              </div>
+                                  {/* <!-- ===== timeline item ===== -->
+                                    <!-- ===== timeline item ===== --> */}
+                              <div className="timeline-item">
+                                <div className="circle-dot"></div>
+                                <h3 className="timeline-date">
+                                  <i className="fa fa-calendar"></i> 2018 - 2018
+                                </h3>
+                                <h4 className="timeline-title">CPUT</h4>
+                                <h5>Teaching Assistant</h5>
+                                <p className="timeline-text">Assisted 35 students with Environmental Impact Assessment assignments.
+                                  Supported 35 students with their map making procedures and assignments.
+                                  Graded assignments for 35 students and ran className help sessions.
+                                  </p>
+                                </div>  
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        </Container>
       </Container>
     </Container> 
 
