e56a917dc92079c0b35bf79dc84f585fdeaaf30f
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-22 15:28:47 +0200

    portfolio

--- Portfolio/src/components/portfolio/Portfolio.jsx
+++ Portfolio/src/components/portfolio/Portfolio.jsx
@@ -3,22 +3,66 @@ import "./portfolio.css";
 import { Container } from 'react-bootstrap';
 
 import image1 from '../../images/skills1.jpg';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Portfolio = () => {
   return (
     <Container className='container-fluid  about-section' id='portfolio'>
-      <div className='portfolio'>
-        <div className='projects'> 
-          <img src= {image1} className='image1'/>
-        </div>
+      <h2 className='title'>Portfolio</h2>
+      <h4>Take a look at some o my pojects:</h4>
 
-        <div className='projects'>
-          <img src= {image1} className='image2'/> 
+        <div className='portfolio'>
+          
+          <div className='projects'>
+            <div>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src={image1} />
+              <Card.Body>
+                <Card.Title>Project 1</Card.Title>
+                <Card.Text>
+                  
+                </Card.Text>
+                <Button variant="primary">Go to project</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          </div>
+          <div className='projects'>
+            <div>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src={image1} />
+              <Card.Body>
+                <Card.Title>Project 2</Card.Title>
+                <Card.Text>
+                  
+                </Card.Text>
+                <Button variant="primary">Go to project</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          </div>
+          <div className='projects'>
+            <div>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src={image1} />
+              <Card.Body>
+                <Card.Title>Project 3</Card.Title>
+                <Card.Text>
+                
+                </Card.Text>
+                <Button variant="primary">Go to project</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          </div>
+
+
         </div>
-        <div className='projects'>
-        <img src= {image1} className='image3'/> 
-        </div>
-      </div>
+         
+         
+           
+    
       
      
 
