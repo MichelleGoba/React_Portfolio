0d551cbe0ba438cebe8bac40cefc864b7b93dd00
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-23 11:20:59 +0200

    project links

--- Portfolio/src/components/portfolio/Portfolio.jsx
+++ Portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import "./portfolio.css";
 import { Container } from 'react-bootstrap';
-
 import image1 from '../../images/skills1.jpg';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import project1 from '../../images/MZAMOMTSHA.png';
+import project2 from '../../images/project2.png';
+import project3 from '../../images/project3.png';
+
 
 const Portfolio = () => {
   return (
     <Container className='container-fluid  about-section' id='portfolio'>
       <h2 className='title'>Portfolio</h2>
-      <h4>Take a look at some o my pojects:</h4>
+      <h4>Take a look at some of my pojects:</h4>
 
         <div className='portfolio'>
           
           <div className='projects'>
             <p>Project 1</p>
-            <img src= {image1}/>
-            <button>More</button>
+            <img src= {project1} className='p-img'/>
+            <a href='https://github.com/MichelleGoba/Mzamomtsha-Primary-School-Website'><button>Go to Project</button></a>
            
           </div>
           <div className='projects'>
             <p>Project 1</p>
-            <img src= {image1}/>
-            <button>More</button>
+            <img src= {project2} className='p-img'/>
+            <a href='https://github.com/MichelleGoba/Ndalo'><button>Go to Project</button></a>
+           
            
           </div>
           <div className='projects'>
             <p>Project 1</p>
-            <img src= {image1}/>
-            <button>More</button>
+            <img src={project3} className='p-img'/>
+            <a href='https://github.com/MichelleGoba/Ndalos_homepage'><button>Go to Project</button></a>
            
           </div>
           
