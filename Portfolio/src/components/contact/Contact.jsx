3ee62e0b6e4fa7adcd3453dbf6cc6eff7cbd7a31
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-23 13:37:18 +0200

    contact form

--- Portfolio/src/components/contact/Contact.jsx
+++ Portfolio/src/components/contact/Contact.jsx
@@ -9,16 +9,16 @@ const Contact = () => {
       <h4>Send me an Email</h4>
       <h4>Im very responsive to messages</h4>
 
-      <div className='contact-container'>
+      <div className='contact-container'>   
         <div className='contact-form'>
           <div className='row'>
-            <div className='group1'>
+            <div className='group'>
               <div className='input'>
               <input type='text' placeholder='Name' className='form-text'></input>
               </div>
             </div>
             
-            <div className='group1'>
+            <div className='group'>
               <div className='input'>
               <input type='text' placeholder='Email' className='form-text'></input>
               </div>
@@ -26,7 +26,7 @@ const Contact = () => {
           </div>
 
           <div className='row'>
-          <div className='group1'>
+          <div className='group'>
               <div className='input'>
               <input type='text' placeholder='Subject' className='form-text'></input>
               </div>
@@ -34,10 +34,16 @@ const Contact = () => {
           </div>
 
           <div className='row'>
-          <div className='group1'>
+          <div className='group'>
               <div className='input'>
               <textarea name="" id="message" className='form-text'  placeholder="Message"></textarea>
               </div>
+            </div>
+          </div>
+
+          <div className='group'>
+            <div className='input'>
+              <buton className='button'></buton>
             </div>
           </div>
 
