5fff52e316fe1a0ee62f2d4f7266aa4e08cf2a37
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-23 12:28:50 +0200

    contact page

--- Portfolio/src/components/contact/Contact.jsx
+++ Portfolio/src/components/contact/Contact.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import "./contact.css";
+import { Container } from 'react-bootstrap';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <Container className='container-fluid  portfolio-section' id='contact'>
+      <h3 className='title'>Contact Me</h3>
+      <h4>Send me an Email</h4>
+      <h4>Im very responsive to messages</h4>
+
+      <div className='contact-container'>
+        <div className='contact-form'>
+          <div className='row'>
+            <div className='group1'>
+              <div className='input'>
+              <input type='text' placeholder='Name' className='form-text'></input>
+              </div>
+            </div>
+            
+            <div className='group1'>
+              <div className='input'>
+              <input type='text' placeholder='Email' className='form-text'></input>
+              </div>
+            </div>
+          </div>
+
+          <div className='row'>
+          <div className='group1'>
+              <div className='input'>
+              <input type='text' placeholder='Subject' className='form-text'></input>
+              </div>
+            </div>
+          </div>
+
+          <div className='row'>
+          <div className='group1'>
+              <div className='input'>
+              <textarea name="" id="message" className='form-text'  placeholder="Message"></textarea>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      
+      
+      </div>
+
+
+
+    </Container>
   )
 }
 
