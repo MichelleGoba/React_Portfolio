e67ea603b76163c73ba0f5e2ecf9f2ea44a9e3f1
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-23 16:07:16 +0200

    Update about, contact, and portfolio sections (generated by useblackbox.ai )

--- Portfolio/src/components/contact/Contact.jsx
+++ Portfolio/src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 
 const Contact = () => {
   return (
-    <Container className='container-fluid  portfolio-section' id='contact'>
+    <Container className='container-fluid  contact-section' id='contact'>
       <h3 className='title'>Contact Me</h3>
       <h4>Send me an Email</h4>
       <h4>Im very responsive to messages</h4>
@@ -12,13 +12,13 @@ const Contact = () => {
       <div className='contact-container'>   
         <div className='contact-form'>
           <div className='row'>
-            <div className='group'>
+            <div className='group1'>
               <div className='input'>
               <input type='text' placeholder='Name' className='form-text'></input>
               </div>
             </div>
             
-            <div className='group'>
+            <div className='group1'>
               <div className='input'>
               <input type='text' placeholder='Email' className='form-text'></input>
               </div>
@@ -26,7 +26,7 @@ const Contact = () => {
           </div>
 
           <div className='row'>
-          <div className='group'>
+          <div className='group2'>
               <div className='input'>
               <input type='text' placeholder='Subject' className='form-text'></input>
               </div>
@@ -34,7 +34,7 @@ const Contact = () => {
           </div>
 
           <div className='row'>
-          <div className='group'>
+          <div className='group2'>
               <div className='input'>
               <textarea name="" id="message" className='form-text'  placeholder="Message"></textarea>
               </div>
@@ -43,7 +43,7 @@ const Contact = () => {
 
           <div className='group'>
             <div className='input'>
-              <buton className='button'></buton>
+              <buton className='btn'>Send Email</buton>
             </div>
           </div>
 
