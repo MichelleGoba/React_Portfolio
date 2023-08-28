89432cdde0430cb7805ad555a8b51d9b25b3845c
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-28 12:53:04 +0200

    contact form validation

--- Portfolio/src/components/contact/Contact.jsx
+++ Portfolio/src/components/contact/Contact.jsx
@@ -1,78 +1,33 @@
 import "./contact.css";
 import { Container } from "react-bootstrap";
-import contact-image from "../../images/contact1";
+import { useState } from "react";
+// import contact1 from '../../images/contact1.png';
 
 const Contact = () => {
+  
+  
   return (
     <Container className="container-fluid  contact-section" id="contact">
       <h3 className="title">Contact Me</h3>
       <h4>Send me an Email</h4>
       <h4>Im very responsive to messages</h4>
 
-      {/* Contact page form start */}
-      
       <div className="contact-container">
         <div className="contact-box">
-          <div className="left">
-            <img src= {contact-image}/>
-          </div>
+          <div className="left"></div>
           <div className="right">
-          <div className="row">
-            <div className="group1">
-              <div className="input">
-                <input
-                  type="text"
-                  placeholder="Name"
-                  className="form-text"
-                ></input>
-              </div>
-            </div>
+            
 
-            <div className="group1">
-              <div className="input">
-                <input
-                  type="text"
-                  placeholder="Email"
-                  className="form-text"
-                ></input>
-              </div>
-            </div>
-          </div>
 
-          <div className="row">
-            <div className="group2">
-              <div className="input">
-                <input
-                  type="text"
-                  placeholder="Subject"
-                  className="form-text"
-                ></input>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="group2">
-              <div className="input">
-                <textarea
-                  name=""
-                  id="message"
-                  className="form-text"
-                  placeholder="Message"
-                ></textarea>
-              </div>
-            </div>
-          </div>
-
-          <div className="group">
-            <button className="cont-btn">Send Email</button>
-          </div>
-
+    
+              
+            
+      
           </div>
         </div>
-        
       </div>
-      {/* Contact page form end */}
+
+
     </Container>
   );
 };
